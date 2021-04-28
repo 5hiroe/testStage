1b265a1ab0d8c59b0634d3b6bbe4d92da9f30e0b
5hiroe <77256143+5hiroe@users.noreply.github.com>
2021-04-28 05:15:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
+//création d'une variable permettant de vérifier que l'on se situe sur l'index lors d'import de components
 $index = 1;
+
+//import du header
 require_once './component/header.php';
+
+//import du formulaire de recherche de disponibilité
 require_once './component/searchform.php';
 ?>
 </body>
